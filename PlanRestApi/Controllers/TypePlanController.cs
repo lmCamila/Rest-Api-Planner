f9f9d3250b1b2b994d2695aee41e9f2138b19415
Camila Lima Martins <camilalm31@gmail.com>
2019-05-14 11:15:34 -0300

    first commit, TypePlan(Get, Post, Put, Delete)

---
+++ PlanRestApi/Controllers/TypePlanController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PlanRestApi.Models;
+using PlanRestApi.Repositories;
+
+namespace PlanRestApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TypePlanController : ControllerBase
+    {
+        private readonly TypePlanRepository typePlanRepository;
+        public TypePlanController(IConfiguration configuration)
+        {
+            typePlanRepository = new TypePlanRepository(configuration);
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var list = typePlanRepository.GetAll();
+            return Ok(list);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            var model = typePlanRepository.Get(id);
+            if(model == null)
+            {
+                return NotFound();
+            }
+            return Ok(model);
+        }
+        [HttpPost]
+        public IActionResult Insert([Bind("Name")]TypePlan type)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = typePlanRepository.Insert(type);
+                var lastType = result ? typePlanRepository.GetLastInserted() : null;
+                var uri = Url.Action("Get", new { id = lastType.Id });
+                return Created(uri, lastType);
+            }
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public IActionResult Update([Bind("Id,Name")]TypePlan type)
+        {
+            if (ModelState.IsValid)
+            {
+                typePlanRepository.Update(type);
+                return Ok();
+            }
+            return BadRequest();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var model = typePlanRepository.Get(id);
+            if(model == null)
+            {
+                return NotFound();
+            }
+            typePlanRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ PlanRestApi/Repositories/TypePlanRepository.cs
@@ -0,0 +1,97 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using PlanRestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanRestApi.Repositories
+{
+    public class TypePlanRepository : AbstractRepository<TypePlan>
+    {
+        public TypePlanRepository(IConfiguration configuration): base(configuration) { }
+        public override bool Delete(int id)
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                if (db.State == ConnectionState.Closed)
+                {
+                    db.Open();
+                }
+                int result = db.Execute("DELETE FROM plan_types WHERE id = @Id", new { Id = id }, commandType: CommandType.Text);
+                return (result > 0);
+            }
+        }
+
+        public override TypePlan Get(int id)
+        {
+
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                if (db.State == ConnectionState.Closed)
+                {
+                    db.Open();
+                }
+                var type = db.QueryFirst<TypePlan>("SELECT * FROM plan_types WHERE id = @Id", new { Id = id });
+                return type;
+            }
+        }
+        public override TypePlan GetLastInserted()
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                if (db.State == ConnectionState.Closed)
+                {
+                    db.Open();
+                }
+                var type = db.QueryFirst<TypePlan>("SELECT * FROM plan_types WHERE id = IDENT_CURRENT('plan_types')");
+                return type;
+            }
+        }
+
+        public override IEnumerable<TypePlan> GetAll()
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                if (db.State == ConnectionState.Closed)
+                {
+                    db.Open();
+                }
+                IEnumerable<TypePlan> types = db.Query<TypePlan>("SELECT * FROM plan_types");
+                return (List<TypePlan>)types;
+            }
+        }
+
+        public override bool Insert(TypePlan item)
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                if (db.State == ConnectionState.Closed)
+                {
+                    db.Open();
+                }
+                int result = db.Execute(@"INSERT INTO plan_types(name)
+                            VALUES(@NAME)", item);
+                return (result > 0);
+            }
+        }
+
+        public override bool Update(TypePlan item)
+        {
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                if (db.State == ConnectionState.Closed)
+                {
+                    db.Open();
+                }
+                int result = db.Execute(@"UPDATE  plan_types 
+                           SET name = @Name
+                           WHERE id = @Id", item);
+                return (result > 0);
+            }
+        }
+    }
+}
